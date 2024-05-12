76836c25faac7ee944e53b4156e1a3ee5e69a8ee
ByDavito <daviracca02@gmail.com>
2024-05-12 16:18:12 -0300

    v 3.07

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,24 +2,15 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-document.addEventListener('DOMContentLoaded', function () {
-    var menuIcon = document.querySelector('.navbar-menu-icon');
-    var offcanvasBody = document.querySelector('.offcanvas-body');
-    var offcanvas = document.getElementById('offcanvasNavbar'); // Accedemos al offcanvas existente
-    var animacion = document.querySelector('.menu__icon');
-  
-    // Movemos el botón al offcanvas cuando se muestra
-    offcanvas.addEventListener('shown.bs.offcanvas', function () {
+function animacionMenu() {
+    var menuIcon = document.getElementById('botonMenu');
+    if (menuIcon.classList.contains('animated')) {
+        menuIcon.classList.remove('animated');
+
+    }
+    else{
+        menuIcon.classList.add('animated');
+    }
     
-      menuIcon.parentNode.removeChild(menuIcon); // Eliminamos el botón de su posición actual
-      offcanvasBody.insertBefore(menuIcon, offcanvasBody.firstChild); // Lo agregamos al offcanvas
-      animacion.focus();
-    });
-  
-    // Movemos el botón al navbar cuando se oculta
-    offcanvas.addEventListener('hidden.bs.offcanvas', function () {
-      menuIcon.parentNode.removeChild(menuIcon); // Eliminamos el botón de su posición actual
-      document.querySelector('.container-fluid').appendChild(menuIcon); // Lo agregamos al navbar
-      animacion.blur();
-    });
-  });
+}
+
