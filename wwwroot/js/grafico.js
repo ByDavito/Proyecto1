fe6263fea22e16fa0ce07ca8d315502484f567cd
ByDavito <davidracca02gmail.com>
2024-05-31 11:51:38 -0300

    v 4.3

--- wwwroot/js/grafico.js
+++ wwwroot/js/grafico.js
@@ -5,25 +5,31 @@ document.addEventListener('DOMContentLoaded', (event) => {
     select.value = currentMonth;
 });
 
-window.onload = mostrarGrafico(), GraficoDonut()
-
 let graficoEjercicio;
-
 let graficoDonutEjercicio;
 
-document.getElementById("MesEjercicioBuscar").addEventListener('change', function () {
-    graficoDonutEjercicio.destroy();
-    graficoEjercicio.destroy();
+$("#MesEjercicioBuscar, #AnioEjercicioBuscar").change(function () {
+    if (graficoDonutEjercicio) graficoDonutEjercicio.destroy();
+    if (graficoEjercicio) graficoEjercicio.destroy();
     mostrarGrafico();
     GraficoDonut();
 });
 
-document.getElementById("AnioEjercicioBuscar").addEventListener('change', function () {
-    graficoEjercicio.destroy();
+$("#TipoEjercicioBuscarID").change(function () {
+    if (graficoEjercicio) graficoEjercicio.destroy();
     mostrarGrafico();
 });
 
+window.onload = function() {
+    // Setear el select en el mes actual
+    const select = document.getElementById('MesEjercicioBuscar');
+    const currentMonth = new Date().getMonth() + 1;
+    select.value = currentMonth;
 
+    // Llamar a las funciones después de setear el select
+    mostrarGrafico();
+    GraficoDonut();
+};
 
 
 
@@ -48,6 +54,8 @@ function mostrarGrafico() {
         // código a ejecutar si la petición es satisfactoria;
         // la respuesta es pasada como argumento a la función
         success: function (ejercicioFisicos) {
+
+            console.log(ejercicioFisicos);
 
             let labels = [];
             let data = [];
@@ -100,7 +108,7 @@ function GraficoDonut(){
     let anio = document.getElementById("AnioEjercicioBuscar").value;
     $.ajax({
         type: "POST",
-        url: '../../EjerciciosFisicos/GraficoCircularEjercicios',
+        url: '../../EjerciciosFisicos/GraficoCircularEjecicios',
         data: {mes: mes, anio: anio},
         success: function (VistaTipoEjercicioFisico) {
            
@@ -122,10 +130,26 @@ function GraficoDonut(){
                 data: {
                     labels: labels,
                     datasets: [{
-                        data: [5,3,6,2,3],
+                        data: data,
                         backgroundColor: fondo,
+                        borderColor: 'transparent',
+                        spacing: 5,
+                        borderRadius: 20,
+                        
                     }],
                 },
+                options: {
+                    responsive: true,
+                    plugins: {
+                        legend: {
+                            position: 'top',
+                        },
+                        tooltip: {
+                            enabled: true
+                        }
+                    },
+                    cutout: '85%'
+                }
             });
         }
     });
@@ -136,10 +160,11 @@ function generarColorRojo() {
     // Los valores de RR y BB serán bajos (de 0 a 127).
 
     let rr = Math.floor(Math.random() * 128) + 128; // 0 a 127
-    let gg = Math.floor(Math.random() * 128); // 128 a 255
-    let bb = Math.floor(Math.random() * 128); // 0 a 127
+    let gg = Math.floor(Math.random() * 75); // 128 a 255
+    let bb = Math.floor(Math.random() * 75); // 0 a 127
 
     // Convertimos a hexadecimal y formateamos para que tenga siempre dos dígitos.
     let colorHex = `#${rr.toString(16).padStart(2, '0')}${gg.toString(16).padStart(2, '0')}${bb.toString(16).padStart(2, '0')}`;
     return colorHex;
 }
+
